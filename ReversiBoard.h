90fee4b973ac81d78e8b21904d41b691afe349e2
Liam Mercier <145395643+LiamMercier@users.noreply.github.com>
2024-04-27 01:18:21 -0700

    Created a function to update the available moves.
    Simplified identification of which player's turn it currently is.
    
    Added check to make_move to skip computation if in setup (which should save some time over the life of each game, as there are at most 64 turns)
    
    Next steps: allow the game to be finished and compare this method against a different method of running the game.

--- ReversiBoard.h
+++ ReversiBoard.h
@@ -9,13 +9,8 @@
 #define BOARD_DIMENSION 8
 #define BOARD_AREA 64
 
-
-
-
-
 class ReversiBoard
 {
-
 public:
 
 	void print_board() const
@@ -54,7 +49,7 @@ public:
 	std::uint8_t make_move(std::uint8_t selection)
 	{
 		// Input validation
-
+		
 		// Check for array bounds
 		if (selection >= BOARD_AREA)
 		{
@@ -83,14 +78,21 @@ public:
 		}
 
 		// place tile and add it to the pool of tiles to search when finding moves
-		board[selection] = (current_turn % 2) + 1;
+		board[selection] = player;
 		active_tiles[tiles_count] = selection;
 		tiles_count++;
 
 		// update other tiles
-		// 
+		
+		// We should skip all this if we are not past turn 4
+		if (current_turn < 4) 
+		{
+			current_turn++;
+			player = (player % 2) + 1;
+			return 1;
+		}
+		
 		// To flip tiles, we must search in all 8 directions.
-
 		for (int i = 0; i < 8; i++)
 		{
 			// for this direction, retrieve the step value to reduce memory access
@@ -116,20 +118,20 @@ public:
 					break;
 				}
 
-				// if this location has an enemy tile, take another step
-				if (board[current_tile] == (2 - current_turn % 2))
+				// if this location has an opponent tile, take another step
+				if (board[current_tile] == (player % 2) + 1)
 				{
 					continue;
 				}
 
 				// if this location has an ally tile, flip all tiles between this tile and selection.
-				if (board[current_tile] == (current_turn % 2) + 1)
+				if (board[current_tile] == player)
 				{
 					while (current_tile != selection) 
 					{
 						std::cout << "BACK STEP OF SIZE: " << +step << " TILE IS AT: " << +current_tile << "\n";
 						current_tile -= step;
-						board[current_tile] = (current_turn % 2) + 1;
+						board[current_tile] = player;
 					}
 					break;
 				}
@@ -138,19 +140,26 @@ public:
 		}
 
 		current_turn++;
+		player = (player % 2) + 1;
 
 	}
 
 	// The same as make_move but without checks for input.
 	//
 	// This can be used for faster computation assuming all
-	// inputs are from the available_moves array. Otherwise, errors
-	// may occur.
-	std::uint8_t make_move_unchecked(int selection)
+	// inputs are from the available_moves array. 
+	// Otherwise, errors may occur.
+	std::uint8_t make_move_unchecked(std::uint8_t selection)
 	{
 
 	}
 
+	// Find valid moves for the current board state.
+	// 
+	// This function updates the class internal "available_moves"
+	// to hold the current moves.
+	//
+	// The values in available_moves may contain duplicates after this operation.
 	void compute_available_moves() 
 	{
 		num_moves = 0;
@@ -161,17 +170,94 @@ public:
 			// add all the tiles that are not occupied
 			for (int i = 0; i < 4; i++)
 			{
-				if (board[initial_moves[i]] == 0) 
+				uint8_t position = initial_moves[i];
+				if (board[position] == 0) 
 				{
-					available_moves[num_moves] = initial_moves[i];
+					available_moves[num_moves] = position;
 					num_moves++;
 				}
 
 			}
+
+			return;
 		}
 
-		// Otherwise, we should search tiles for potential moves
+		// If turn >= 4 we should search tiles for potential moves
+		//
+		// Iterate over active_tiles and check if the tile is owned by the player
+		// If so, search for a line of opponent tiles that ends in an empty space
 
+		for (int i = 0; i < tiles_count; i++) 
+		{
+			uint8_t position = active_tiles[i];
+
+			// If not a player tile, skip to next one
+			if (board[position] != player) 
+			{
+				continue;
+			}
+
+			// Search each direction for opponent tile
+
+			for (int i = 0; i < 8; i++) 
+			{
+				// Retrieve the step value
+				std::int8_t step = directions[i];
+				std::uint8_t current_tile = position;
+
+				// Set true if we found a valid line to save
+				bool valid_flag = false;
+
+				while (true) 
+				{
+					// Take a step in the current direction
+					current_tile += step;
+
+					// If this location is out of bounds, stop searching
+					if (current_tile >= BOARD_AREA)
+					{
+						break;
+					}
+
+					// If this location has an empty tile then this
+					// Tile is a move if we have taken a step
+					if (board[current_tile] == 0)
+					{
+						if (valid_flag) 
+						{
+							// Add to list of moves
+							available_moves[num_moves] = current_tile;
+							num_moves++;
+						}
+						break;
+					}
+
+					// If this location has an opponent tile, take another step
+					if (board[current_tile] == (3 - player))
+					{
+						// Unless we are on an edge
+						if (current_tile % 8 == 0)
+						{
+							break;
+						}
+
+						if (current_tile % 8 == 7)
+						{
+							break;
+						}
+						
+
+						valid_flag = true;
+						continue;
+					}
+
+					// else, break
+					break;
+
+				}
+			}
+
+		}
 	}
 
 
@@ -180,57 +266,41 @@ private:
 	// board contains the current state of the reversi game
 	std::uint8_t board[BOARD_AREA]{};
 	std::uint8_t current_turn = 0;
+	std::uint8_t player = 1;
 
-	// occupied tiles is a list of tiles currently in play
+	// active_tiles contains tiles currently in play
 	std::uint8_t active_tiles[BOARD_AREA]{};
 	std::uint8_t tiles_count = 0;
 
-	// this array holds the valid moves for the current player
-	// the array should be wiped (set to BOARD_AREA).
+	// available_moves holds the valid moves for the current player
+	// num_moves allows us to avoid clearing the whole array each time we search.
 	std::uint8_t available_moves[BOARD_AREA];
 	std::uint8_t num_moves = 0;
 
-	// static lookup table for first 4 moves
+	// Static lookup table for first 4 moves
 	//
-	// the top left tile is (BOARD_DIMENSION/2) - 1 tiles down, (BOARD_DIMENSION/2)-1 tiles right
-	// the top right tile is one to the right of that
-	// the bottom left tile is BOARD_DIMENSION/2 tiles down, (BOARD_DIMENSION/2) - 1 tiles right
-	// the bottom right tile is one to the right of that
+	// The top left tile is (BOARD_DIMENSION/2) - 1 tiles down, (BOARD_DIMENSION/2)-1 tiles right
+	// The top right tile is one to the right of that
+	// The bottom left tile is BOARD_DIMENSION/2 tiles down, (BOARD_DIMENSION/2) - 1 tiles right
+	// The bottom right tile is one to the right of that
 	//
-	// simplified, if BOARD_DIMENSION is B
+	// Simplified, if BOARD_DIMENSION is B
 	//
-	// TopLeft = (B^2 - B)/2 - 1
-	// TopRight = (B^2 - B)/2
-	// BottomLeft = (B^2 + B)/2 - 1
-	// BottomRight = (B^2 + B)/2
+	// Top Left = (B^2 - B)/2 - 1
+	// Top Right = (B^2 - B)/2
+	// Bottom Left = (B^2 + B)/2 - 1
+	// Bottom Right = (B^2 + B)/2
 	static constexpr std::uint8_t initial_moves[4]{ (BOARD_DIMENSION * BOARD_DIMENSION - BOARD_DIMENSION) / 2 - 1, (BOARD_DIMENSION * BOARD_DIMENSION - BOARD_DIMENSION) / 2,
 													(BOARD_DIMENSION * BOARD_DIMENSION + BOARD_DIMENSION) / 2 - 1, (BOARD_DIMENSION * BOARD_DIMENSION + BOARD_DIMENSION) / 2 };
 
 
-	// static lookup table for directions
+	// Static lookup table for directions
 	//
-	// Moving up results in a change of -BOARD_DIMENSION to the current tile value, down results in +BOARD_DIMENSION, 
-	// left results in -1, right results in +1, thus, we can compute each direction easily
+	// The first element is the direction for north-west, the rest are filled in clockwise.
 	static constexpr std::int8_t directions[8]{ -BOARD_DIMENSION - 1, -BOARD_DIMENSION, -BOARD_DIMENSION + 1,
-												-1,															1,
-												BOARD_DIMENSION - 1, BOARD_DIMENSION, BOARD_DIMENSION + 1 };
+												-1,                                                        1,
+												 BOARD_DIMENSION - 1,  BOARD_DIMENSION,  BOARD_DIMENSION + 1 };
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
